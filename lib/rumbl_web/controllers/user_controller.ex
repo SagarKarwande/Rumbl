50e41ab1eeb8cb14aa9d33c66775dabf7a7a257d
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-09-18 17:12:42 +0530

    Show single user

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -6,4 +6,9 @@ defmodule RumblWeb.UserController do
       users = Repo.all(Rumbl.User)
       render conn, "index.html", users: users
     end
+
+    def show(conn, %{ "id" => id }) do
+      user = Repo.get(Rumbl.User, id)
+      render conn, "show.html", user: user
+    end
 end
