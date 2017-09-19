3fb7314cfd6969f6c4dbeb2149e3a49012782294
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-09-19 14:05:38 +0530

    Add new user functionality

--- lib/rumbl_web/models/user.ex
+++ lib/rumbl_web/models/user.ex
@@ -5,8 +5,15 @@ defmodule Rumbl.User do
     field :name, :string
     field :username, :string
     field :password, :string, virtual: true
-    field :password_hash, :string 
-    timestamps
+    field :password_hash, :string
+
+    timestamps()
   end
 
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
